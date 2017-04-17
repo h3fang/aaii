e68366335d424f0627d236a31916fc2578371ed0
Enigma <hf.enigma@gmail.com>
2017-04-17 07:13:38 +0000

    flash messages

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -33,6 +33,7 @@
   
   <body>
     @include('partials.navbar')
+    @include('partials.flash')
     
     @yield('content')
     
@@ -47,6 +48,10 @@
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
     
+    <script>
+        $('#flash-alert').not('.alert-danger').delay(3000).fadeOut(350);
+    </script>
+
     @yield('javascript')
     
   </body>
